77288361824c85424828760046fb2eb919cc13ee
David Iglesias Teixeira <ditman@gmail.com>
2023-12-06 22:06:08 -0800

    Tweak example app.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,67 +16,105 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final _flutterAppIconsPlugin = FlutterAppIcons();
-  String icon = 'favicon.png';
+  String _icon = 'favicon.png';
+  Timer? _autoChange;
+
   @override
   void initState() {
     super.initState();
-    //_flutterAppIconsPlugin.setIcon(icon: 'favicon-failure.png');
+  }
+
+  // Periodically changes the icon every 2 seconds.
+  void startTimer() {
+    // Toggle icon change every 2 seconds, just for testing.
+    final Timer timer =
+        Timer.periodic(const Duration(seconds: 2), (Timer timer) {
+      final String nextIcon = getOldIcon(_icon);
+      setIcon(nextIcon);
+    });
+    setState(() {
+      _autoChange = timer;
+    });
+  }
+
+  // Stops changing the icon every 2 seconds.
+  void stopTimer() {
+    _autoChange?.cancel();
+    setState(() {
+      _autoChange = null;
+    });
+  }
+
+  // Sets the icon with the `_flutterAppIconsPlugin`
+  void setIcon(String? nextIcon) {
+    if (nextIcon == null) return;
+    _flutterAppIconsPlugin.setIcon(
+      icon: nextIcon,
+      oldIcon: getOldIcon(nextIcon),
+    );
+    setState(() {
+      _icon = nextIcon;
+    });
+  }
+
+  // Computes the old icon for a given icon.
+  String getOldIcon(String icon) {
+    if (icon == 'favicon.png') {
+      return 'favicon-failure.png';
+    }
+    return 'favicon.png';
   }
 
   @override
   Widget build(BuildContext context) {
-    //Auto icon change every 5 seconds, just for testing.
-    Timer.periodic(const Duration(seconds: 5), (Timer timer) {
-      String oldIcon = icon;
-      if (icon == 'favicon.png') {
-        icon = 'favicon-failure.png';
-      } else {
-        icon = 'favicon.png';
-      }
-      _flutterAppIconsPlugin.setIcon(icon: icon, oldIcon: oldIcon);
-    });
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('FlutterAppIcons example'),
+          surfaceTintColor: Theme.of(context).colorScheme.primary,
         ),
         body: Center(
-          child: SizedBox(
-            width: 140,
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 200, 0, 0),
-              child: Column(
-                children: [
-                  Text('Change favicon here:'),
-                  Row(
-                    children: [
-                      TextButton(
-                        onPressed: () {
-                          _flutterAppIconsPlugin.setIcon(
-                            icon: 'favicon-failure.png',
-                            oldIcon: 'favicon.png',
-                          );
-                        },
-                        child: const Text(
-                          'Red',
-                          style: TextStyle(color: Colors.red),
-                        ),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          _flutterAppIconsPlugin.setIcon(
-                            icon: 'favicon.png',
-                            oldIcon: 'favicon-failure.png',
-                          );
-                        },
-                        child: const Text(
-                          'Blue',
-                        ),
-                      ),
-                    ],
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 500),
+            child: ListView(
+              shrinkWrap: true,
+              children: [
+                const ListTile(
+                  title: Text('Select favicon:'),
+                ),
+                RadioListTile<String>(
+                  value: 'favicon.png',
+                  groupValue: _icon,
+                  onChanged: _autoChange == null ? setIcon : null,
+                  title: const Text(
+                    'Blue',
+                    style: TextStyle(color: Colors.blue),
                   ),
-                ],
-              ),
+                ),
+                RadioListTile<String>(
+                  value: 'favicon-failure.png',
+                  groupValue: _icon,
+                  onChanged: _autoChange == null ? setIcon : null,
+                  title: const Text(
+                    'Red',
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                const ListTile(
+                  title: Text('Automatic mode:'),
+                ),
+                SwitchListTile(
+                  title: const Text('Change every 2 seconds'),
+                  value: _autoChange != null,
+                  onChanged: (bool value) {
+                    if (value) {
+                      startTimer();
+                    } else {
+                      stopTimer();
+                    }
+                  },
+                )
+              ],
             ),
           ),
         ),
